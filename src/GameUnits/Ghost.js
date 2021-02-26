4f0097ce3c42e3c56ce6aa2005bce0e3fd358593
Stanimir Genkov <s.genkov@wienergames.com>
2021-02-26 11:10:18 +0200

    MU

--- src/GameUnits/Ghost.js
+++ src/GameUnits/Ghost.js
@@ -4,9 +4,7 @@ import GameUnit from './GameUnit';
 export default class Ghost extends GameUnit {
     constructor(unitName, color) {
         super(unitName, color);
-        DC.objectsCreation && console.log("Ghost created"); //^ FLOW
         this.behaviours = ["player1", "move"];
-        this.rect = new Rectangle(this.currentNode.position.x, this.currentNode.position.y, 12, 12);
     };
 
 };
--- src/GameUnits/Pacman.js
+++ src/GameUnits/Pacman.js
@@ -4,9 +4,7 @@ import GameUnit from './GameUnit';
 export default class Pacman extends GameUnit {
     constructor(unitName) {
         super(unitName);
-        DC.objectsCreation && console.log("Pacman created"); //^ FLOW
         this.behaviours = ["player1", "move", "updateInfo"]; 
-        this.rect = new Rectangle(this.currentNode.position.x, this.currentNode.position.y, 12, 12);
         this.createInfo(); //^ For coordinates tracking
     };
 
