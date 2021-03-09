6ff469a0c1af2e6ccac9d26f413d79e3fa0cb01f
Stanimir Genkov <s.genkov@wienergames.com>
2021-03-09 12:28:42 +0200

    MU

--- src/Dot/DotManager.js
+++ src/Dot/DotManager.js
@@ -26,26 +26,16 @@ export default class DotManager {
                 if (node.edges[buildDirection] === undefined) return;
                 let max;
                 const dotsGap = 22;
-                if (node.edges.hasOwnProperty(buildDirection)) {
-                    if (buildDirection === "right") {
-                        max = map.find(el => el.id === node.edges[buildDirection]).position.x;
-                        for (let i = 0; (node.position.x + dotsGap * i) < max ; ++i) {
-                            const newPosition = {
-                                ...node.position,
-                                x: node.position.x + dotsGap * i,
-                            };
-                            new Dot(newPosition);
+                if (buildDirection in node.edges) { // node.edges.hasOwnProperty(buildDirection)
+                    const currentDirection = buildDirection === "right" ? "x" : "y";
+                    max = map.find(el => el.id === node.edges[buildDirection]).position[currentDirection];
+                    for (let i = 0; (node.position[currentDirection] + dotsGap * i) < max + 1; ++i) {
+                        const newPosition = {
+                            ...node.position,
+                            [currentDirection]: node.position[currentDirection] + dotsGap * i
                         };
-                    } else {
-                        max = map.find(el => el.id === node.edges[buildDirection]).position.y;
-                        for (let i = 0; (node.position.y + dotsGap * i) <= max; ++i) {
-                            const newPosition = {
-                                ...node.position,
-                                y: node.position.y + dotsGap * i
-                            };
-                            new Dot(newPosition);
-                        };
-                    }
+                        new Dot(newPosition);
+                    };
 
 
                 };
